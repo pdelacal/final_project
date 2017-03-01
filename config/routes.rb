cb1af132e8a91d6d25f6d58808c11f2862fd2d5a
Nick Duncan <nickeduncan@gmail.com>
2017-03-01 18:36:49 -0500

    add page controller with index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root 'page#index'
+
+  get '/register' => 'users#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
