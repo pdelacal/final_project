618f76a2da5e8d0d9090d2cf054a7d72efb318cf
Nick Duncan <nickeduncan@gmail.com>
2017-03-01 18:39:39 -0500

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   get '/register' => 'users#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
 end
