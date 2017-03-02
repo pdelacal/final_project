7f5eaf3bcf5795bdba5200900935466aefceb1a7
Nick Duncan <nickeduncan@gmail.com>
2017-03-01 19:22:01 -0500

    added welcome page after login

--- spec/features/visiting_root_spec.rb
+++ spec/features/visiting_root_spec.rb
@@ -7,5 +7,14 @@ feature 'Visting Root' do
     expect(current_path).to eq('/register')
   end
 
-  it 'when logged in shows a welcome message'
+  it 'when logged in shows a welcome message' do
+    user = User.create(name: 'John Doe', email: 'john@me.com', password: 'password')
+    visit login_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Login'
+    visit root_path
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('Welcome')
+  end
 end
