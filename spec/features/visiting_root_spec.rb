e7ec08a1af0ab5541b1d1c1c0608784b7e98df88
Nick Duncan <nickeduncan@gmail.com>
2017-03-03 02:29:52 -0500

    Removed rspec for required registration

--- spec/features/visiting_root_spec.rb
+++ spec/features/visiting_root_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 feature 'Visting Root' do
 
-  it 'when not logged in redirects to a registration page' do
-    visit root_path
-    expect(current_path).to eq('/register')
-  end
+  # it 'when not logged in redirects to a registration page' do
+  #   visit root_path
+  #   expect(current_path).to eq('/register')
+  # end
 
   it 'when logged in shows a welcome message' do
     user = User.create(name: 'John Doe', email: 'john@me.com', password: 'password')
