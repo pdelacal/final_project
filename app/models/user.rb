3611b461412f2efcee0614a266f37ad1c98a3430
Philip de la Cal <pdelacal@gmail.com>
2017-03-04 15:57:59 -0500

    started Facebook authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,15 @@ class User < ApplicationRecord
               join_table: :requests_to,
               foreign_key: :to_user_id,
               association_foreign_key: :from_user_id
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
