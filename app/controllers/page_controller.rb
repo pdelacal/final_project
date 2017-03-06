b7fb8dc640dd99232245c2a2ce02b09bf3db3c6f
Nick Duncan <nickeduncan@gmail.com>
2017-03-06 16:01:55 -0500

    added search bar that returns results based on a user's name or a listing's city

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,10 @@
 class PageController < ApplicationController
   def friends
   end
+
+  def search
+    @users = User.where "name LIKE ?", "%#{params[:term]}%"
+    @listings = Listing.where "city LIKE ?", "%#{params[:term]}%"
+    # @results = users + listings
+  end
 end
