4f7ff5680a2ccfcb37b292e96a3105bb9eb70371
Nick Duncan <nickeduncan@gmail.com>
2017-03-07 02:13:29 -0500

    fixed bug in listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /listings
   # GET /listings.json
@@ -19,13 +20,14 @@ class ListingsController < ApplicationController
 
   # GET /listings/1/edit
   def edit
+    @listing = current_user.listing
   end
 
   # POST /listings
   # POST /listings.json
   def create
-    @listing = Listing.new(listing_params)
-    current_user.listing = @listing
+    current_user.listing = Listing.new(listing_params)
+    @listing = Listing.find_by(user_id: current_user.id)
 
     respond_to do |format|
       if @listing.save
@@ -41,15 +43,15 @@ class ListingsController < ApplicationController
   # PATCH/PUT /listings/1
   # PATCH/PUT /listings/1.json
   def update
-    @listing = Listing.new(listing_params)
-    current_user.listing = @listing
-    
+    current_user.listing = Listing.new(listing_params)
+    @listing = Listing.find_by(user_id: current_user.id)
+
     respond_to do |format|
-      if @listing.update(listing_params)
-        format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+      if @listing.save
+        format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
+        format.json { render :show, status: :created, location: @listing }
       else
-        format.html { render :edit }
+        format.html { render :new }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -60,7 +62,7 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Listing was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -68,11 +70,24 @@ class ListingsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
-      @listing = Listing.find(params[:id])
+      @listing = Listing.find_by(user_id: params[:id])
+    end
+    #
+    def set_user
+      @user = current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.permit(:address, :rent, :apt_number, :city, :state, :zipcode, :amenities, :rules, :image_url, :description)
+
+      params.require(:listing).permit(
+        :address,:rent,:amenities,:image_url,:description,:apt_number,:city,
+        :state,:zipcode,:deposit,:move_in_date,:stay_duration,:in_unit_washer,
+        :wifi_included,:utilities_included,:furnished,:elevator,:doorman,
+        :air_conditioning,:heating,:month_to_month,:gym,:tv,:own_closet,
+        :pet_friendly,:private_bathroom,:kitchen,:no_smoking,:no_pets,
+        :no_loud_music,:no_overnight_guests,:no_drugs,:no_late_nights,
+        :no_drinking,:no_house_guests
+      )
     end
 end
