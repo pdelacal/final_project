1cd86bec9c5ae5411d53ebe8d73d79d77af5cc89
Nick Duncan <nickeduncan@gmail.com>
2017-03-09 01:09:51 -0500

    map only displays other users' listings

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,7 +5,7 @@ class PageController < ApplicationController
   def search
     # @results = "REGEXP_MATCHES(city, ?)", "#{params[:term.split(",").first]}"
     if [:term]
-    @listings = Listing.where("city LIKE ?", "%#{params[:term].split(",").first}%")
+    @listings = Listing.where("city LIKE ?", "%#{params[:term].split(",").first}%") 
   end
     # @users = User.where "name LIKE ?", "%#{params[:term]}%"
     # Listing.where("address LIKE ?", "%#{params[:term]}%")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306151327) do
+ActiveRecord::Schema.define(version: 20170309043517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 20170306151327) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "facebook_id"
+    t.string   "picture"
   end
 
   add_foreign_key "listings", "users"
