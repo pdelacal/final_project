26eec00b813773fb268fdaefa52b818aa0053568
Nick Duncan <nickeduncan@gmail.com>
2017-03-09 15:58:03 -0500

    fixed bug in search results where tried to render listing's user without listing

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -5,7 +5,12 @@ class PageController < ApplicationController
   def search
     # @results = "REGEXP_MATCHES(city, ?)", "#{params[:term.split(",").first]}"
     if [:term]
-    @listings = Listing.where("city LIKE ?", "%#{params[:term].split(",").first}%") 
+    @listings = Listing.where("city LIKE ?", "%#{params[:term].split(",").first}%")
+    @listings.each do |listing|
+      if listing.user_id == current_user.id
+        @listings.delete(listing)
+      end
+    end
   end
     # @users = User.where "name LIKE ?", "%#{params[:term]}%"
     # Listing.where("address LIKE ?", "%#{params[:term]}%")
