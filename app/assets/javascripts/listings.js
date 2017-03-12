b321a18fce333e13fba0fa99557114792fc34c2a
Nick Duncan <nickeduncan@gmail.com>
2017-03-12 16:38:43 -0400

    listing photos display in carousel on listing page

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,1 +1,27 @@
-$(".owl-carousel").owlCarousel();
+$(document).on('turbolinks:load', function() {
+  $('.glyphicon-chevron-left').on('click', function() {
+    if($('.photo-slide-1').hasClass('active')) {
+      $('.photo-slide-1').removeClass('active');
+  	  $('.photo-slide-3').addClass('active');
+    } else if($('.photo-slide-3').hasClass('active')) {
+      $('.photo-slide-3').removeClass('active');
+  	  $('.photo-slide-2').addClass('active');
+    } else if ($('.photo-slide-2').hasClass('active')) {
+      $('.photo-slide-2').removeClass('active');
+    	$('.photo-slide-1').addClass('active');
+    }
+  });
+
+  $('.glyphicon-chevron-right').on('click', function() {
+    if($('.photo-slide-1').hasClass('active')) {
+      $('.photo-slide-1').removeClass('active');
+      $('.photo-slide-2').addClass('active');
+    } else if($('.photo-slide-3').hasClass('active')) {
+      $('.photo-slide-3').removeClass('active');
+      $('.photo-slide-1').addClass('active');
+    } else if ($('.photo-slide-2').hasClass('active')) {
+      $('.photo-slide-2').removeClass('active');
+      $('.photo-slide-3').addClass('active');
+    }
+  });
+});
