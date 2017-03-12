56fcf9fb44419ce0e6c116a17a7f2f2856f50487
Nick Duncan <nickeduncan@gmail.com>
2017-03-12 17:20:30 -0400

    fixed issues with spacing on search results

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,15 +1,15 @@
 $(document).on('turbolinks:load', function() {
   $('.glyphicon-chevron-left').on('click', function() {
-    if($('.photo-slide-1').hasClass('active')) {
-      $('.photo-slide-1').removeClass('active');
-  	  $('.photo-slide-3').addClass('active');
-    } else if($('.photo-slide-3').hasClass('active')) {
-      $('.photo-slide-3').removeClass('active');
-  	  $('.photo-slide-2').addClass('active');
-    } else if ($('.photo-slide-2').hasClass('active')) {
-      $('.photo-slide-2').removeClass('active');
-    	$('.photo-slide-1').addClass('active');
-    }
+      if($('.photo-slide-1').hasClass('active')) {
+        $('.photo-slide-1').removeClass('active');
+    	  $('.photo-slide-3').addClass('active');
+      } else if($('.photo-slide-3').hasClass('active')) {
+        $('.photo-slide-3').removeClass('active');
+    	  $('.photo-slide-2').addClass('active');
+      } else if ($('.photo-slide-2').hasClass('active')) {
+        $('.photo-slide-2').removeClass('active');
+      	$('.photo-slide-1').addClass('active');
+      }
   });
 
   $('.glyphicon-chevron-right').on('click', function() {
