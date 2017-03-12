a96f34f823c40c76202d39e9df174d650cd57f3b
Philip de la Cal <pdelacal@gmail.com>
2017-03-11 22:09:19 -0500

    Fixed master(for me) by removing avatars and migrating with picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308190122) do
+ActiveRecord::Schema.define(version: 20170312024225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 20170308190122) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "facebook_id"
+    t.string   "picture"
   end
 
   add_foreign_key "listingphotos", "listings"
