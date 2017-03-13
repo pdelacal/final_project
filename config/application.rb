9189d5e70270de051df3a1bfe5849ba2f9b61503
Nick Duncan <nickeduncan@gmail.com>
2017-03-13 17:51:44 -0400

    compiled asset path for heroku

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Apartment
     # config.session_store :cookie_store, key: '_interslice_session'
     # config.middleware.use ActionDispatch::Cookies # Required for all session management
     # config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+
+    config.assets.paths << Rails.root.join('/app/assets/*')
+    config.assets.enabled = true
   end
 
 end
